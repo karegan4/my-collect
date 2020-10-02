e077e0a7b31aa0663ea85dda9ab0637b8e6550d3
Kaitlin Regan <karegan@gmail.com>
2020-10-02 14:07:24 -0400

    done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
+def my_collect(collection)
+i = 0
+result = []
+while i < collection.length
+    result.push yield (collection[i])
+    i += 1
+end
+result
+end
 
-
+#capstring = []
+#my_collect(collection) do |capitalize|
+#    capstring << capitalize.upcase
+#end
+#capstring
